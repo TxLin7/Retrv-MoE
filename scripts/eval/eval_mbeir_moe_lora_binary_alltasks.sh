22d8fde07ed67cd1277a5d3ae5442366c54bbba9
TxLin7 <83452409+TxLin7@users.noreply.github.com>
2026-06-02 11:48:41 +0800

    Update eval_mbeir_moe_lora_binary_alltasks.sh

--- scripts/eval/eval_mbeir_moe_lora_binary_alltasks.sh
+++ scripts/eval/eval_mbeir_moe_lora_binary_alltasks.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-MODEL_ID="${/checkpoints/qwen2-vl-2b_mbeir_moe_lora_stage3_2e4_4gpu_1epoch_train_val_bestv3_2temp_32bs_4ex_top2_128Rank_multigpu}"
-ORIGINAL_MODEL_ID="${ORIGINAL_MODEL_ID:-/njfs/train-ali/tongxu/weibo_X2X/checkpoints/LamRA-Ret-merged-78}"
-IMAGE_PATH_PREFIX="${IMAGE_PATH_PREFIX:-/njfs/train-ali/tongxu/weibo_X2X/data_binary}"
-GLOBAL_POOL="${GLOBAL_POOL:-/njfs/train-ali/tongxu/weibo_X2X/data_binary/cand_pool/global/mbeir_union_test_cand_pool_bin.jsonl}"
-INSTRUCTIONS="${INSTRUCTIONS:-/njfs/train-ali/tongxu/weibo_X2X/data_binary/instructions/query_instructions.tsv}"
-TASK_CONFIG="${TASK_CONFIG:-./eval/eval_tasks.json}"
-CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-4,5,7}"
-MAIN_PROCESS_PORT="${MAIN_PROCESS_PORT:-29509}"
-BATCH_SIZE="${BATCH_SIZE:-32}"
+MODEL_ID=/path/to/retrv-moe-2b-4ex-top2
+ORIGINAL_MODEL_ID=/path/to/base/model
+IMAGE_PATH_PREFIX=""
+GLOBAL_POOL=""
+INSTRUCTIONS=query_instructions.tsv
+TASK_CONFIG=./eval/eval_tasks.json
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+MAIN_PROCESS_PORT=29509
+BATCH_SIZE=32
 
 CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES}" accelerate launch --multi_gpu --main_process_port "${MAIN_PROCESS_PORT}" eval/eval_mbeir_moe_lora_binary_alltasks.py \
     --task_config "${TASK_CONFIG}" \
